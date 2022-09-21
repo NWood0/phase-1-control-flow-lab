d7c3acea613b0a83798628f84b6a90dace09cc8b
Nazire <nazirej@gmail.com>
2022-09-21 11:27:18 -0400

    commit tree f63e1c47b9620484f1872ebdd0ad75f6c9f0cf3f

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(number){
   // Write your code here!
+let result
+if (number <= 400) {
+  result = 'This one is on me!'
+  return result
+} else if (number <= 2000) {
+result = 'That will be twenty bucks.'
+return result}
+else if (number >= 2000 && number <= 2500) {
+  result = 'I will gladly take your thirty bucks.'
+  return result
+} else {result = 'No can do.'
+return result}
 }
-
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let result
+  if (city == 'NYC')
+{result = "Ok, sounds good."
+return result}
+else { result = "No go." } 
+return result
 }
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let response
+  switch (tip) {
+    case 'generous':
+      response = 'Thank you so much.';
+    return response
+    case 'not as generous':
+      response = 'Thank you.'
+      return response 
+    default: 'Bye.'
+    response = 'Bye.'
+    return response
+  }
 }
